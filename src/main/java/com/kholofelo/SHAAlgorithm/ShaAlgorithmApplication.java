ebd7c80c25280e714132561e65b4c71d69fd7d80
kholofelo Ramile <kholofelo.ramile@xgileit.com>
2021-12-31 08:56:23 +0200

    SHA-256 initial commit

---
+++ src/main/java/com/kholofelo/SHAAlgorithm/ShaAlgorithmApplication.java
@@ -0,0 +1,13 @@
+package com.kholofelo.SHAAlgorithm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShaAlgorithmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShaAlgorithmApplication.class, args);
+	}
+
+}
